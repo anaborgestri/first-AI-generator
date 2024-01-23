977d1bc5d9edfd65229302e4e841e2266e4a4e81
anaborgestri <aborges318@gmail.com>
2024-01-23 18:58:06 +0100

    add AI Api

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function typemeaPoem(event) {
-  event.preventDefault();
+function tellaPoem(response) {
+  console.log(response.data.answer);
 
   new Typewriter("#poem", {
-    strings: "Bate leve, levemnte, Como quem chama por mim",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function typemeaPoem(event) {
+  event.preventDefault();
+
+  let usersinstructions = document.querySelector("#user-instructions");
+  let apiKey = "4c08634eb8b52t7acf769o96f5812f64";
+  let prompt = `The user word or theme is this one ${usersinstructions.value}`;
+  let context =
+    "Hello AI generator. You are a well-known romantic AI that is aware of the most famous and beautiful poems in the world. Please generate a short poem, with maximum of 10 lines, tranlated in english, adding each setence in a separate line using the key word or theme requested by the user. Type the poem in HTML format and in the end sign it off with your name in bold and italic letters 'AI the Poet'.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios(apiUrl).then(tellaPoem);
 }
 
 let poemformElement = document.querySelector("#poem-form");
